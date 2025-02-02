2ea4b71378c4897ecbe9010bc876fb034e4b4e6e
Thunderer <grom10091@gmail.com>
2025-02-02 17:30:12 +0300

    use mantine

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,16 @@
 import { Metadata } from "next";
+import { MantineProvider } from "@mantine/core";
+import { Button } from "@mantine/core";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
 export default function Page() {
-  return <p>Customers Page</p>;
+  return (
+    <MantineProvider>
+      <p>Customers Page</p>
+      <Button>Click heare</Button>
+    </MantineProvider>
+  );
 }
